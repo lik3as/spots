08bf1412e94f6fb2f7196ab808408de26f045176
lik3as <daviacosta2006@gmail.com>
2024-12-11 19:17:39 -0300

    added dist to gitignore

--- dist/infra/controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Controller {
-    constructor(operations) {
-        this.operations = operations;
-    }
-}
-exports.default = Controller;
