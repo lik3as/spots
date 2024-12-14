5c63b62c1cf212f7a91dfcaf05402f3cee41e38c
lik3as <daviacosta2006@gmail.com>
2024-12-14 14:20:09 -0300

    feat: added scpeficiOperations type

--- src/types/operations.ts
+++ src/types/operations.ts
@@ -1,5 +1,6 @@
 import handler from "./handler";
 
 type operations = Map<`GET:${string}`, handler>;
+export type specificOperations = Map<`GET:${string}`, handler | "get-view">;
 
 export default operations;
