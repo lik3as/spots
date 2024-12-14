e298573bb00b7916e3fe585dce17ccb3977cbafa
lik3as <daviacosta2006@gmail.com>
2024-12-14 14:21:40 -0300

    feat: Konder and ArtGallery

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import ArtGalleryController from "./controllers/ArtGalleryController";
 import BlessedController from "./controllers/BlessedController";
 import HomeController from "./controllers/HomeController";
 import HubController from "./controllers/HubController";
+import KonderController from "./controllers/KonderController";
+import SktparkController from "./controllers/SktparkController";
 import AppEngine from "./infra/AppEngine";
-
 
 AppEngine.init([
   new HubController(), new HomeController(),
-  new BlessedController()
+  new BlessedController(), new SktparkController(),
+  new KonderController(), new ArtGalleryController()
 ]);
